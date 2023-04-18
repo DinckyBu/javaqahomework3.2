f2229f283a099dd6886d57b135c9084b645d3be2
Екатерина Кобелева <kobelevaekaterina09@gmail.com>
2023-04-18 21:30:09 +0300

    Added project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.lang.module.FindException;
+
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double heightInMeters  = 1.87;
+        int weightInKg = 98;
+        int bmi = service.calculate(heightInMeters,weightInKg);
+        System.out.println(bmi);
+    }
+}
